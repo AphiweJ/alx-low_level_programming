4c6a85a66a081370ba118d026d447640abcbf618
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-14 12:42:37 -0700

    Program that assigns a random number to n

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lowercase
+ *
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	putchar("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"\n);
+	return (0);
+}
