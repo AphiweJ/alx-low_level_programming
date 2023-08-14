120a1fb7833e76ff00d175b8da2487a3754448ac
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-14 03:35:26 -0700

    Fifth commit: 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: Program that prints a statement
+ * Return: 0 (Sucessful)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
