70e38f365503606ae610bf066d61d5da45c19ac1
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-14 04:01:25 -0700

    Sixth commit: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints a statement followed by a new line
+ *
+ * Return: 0 (Sucessful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
