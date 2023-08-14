d57f43790cae8e1ed8d1b6505c2e76b0912587e1
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-14 06:23:46 -0700

    Last commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints a statement followed by a new line to std error
+ * Return: 1 (Successful)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19);
+	return (1);
+}
