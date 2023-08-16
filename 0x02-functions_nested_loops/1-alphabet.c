cef997d2371d4f5e11b1679786ba97bb49be8283
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-16 11:17:45 -0700

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (alpha[i] != 0)
+	{
+
+		putchar(alpha[i]);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
