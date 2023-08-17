b91215548a8284ed59654670700c3f20e8d20290
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-17 05:41:53 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints alphabet 10 times
+ * print_alphabet_x10 - Prints alphabet 10 times
  *
  * Return: 0 (Success)
  */
-int main(void)
+void print_alphabet_x10(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
