8a10621b11f64925fae36eeec1f378dd3cd042c0
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-17 05:59:35 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (j = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 97; j <= 122; j++)
+		for (j = 0; j <= 26; j++)
 		{
-		putchar(alpha[i]);
+		putchar(alpha[j]);
 		}
 
 	putchar('\n');
