5eb5319444779b46e4b62322a63fa056cad6e46d
Aphiwe Joni <joniaphiwe20@gmail.com>
2023-08-18 15:32:27 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,8 +7,8 @@
  * @n: Used as a condition that determines which sign to be printed
  *
  * Return: 1 and prints + if n > 0
- *         0 and prints 0 if n is 0
- *        -1 and prints - if n < 0
+ *	 0 and prints 0 if n is 0
+ *	-1 and prints - if n < 0
  */
 
 int print_sign(int n)
@@ -18,11 +18,12 @@ int print_sign(int n)
 
 		_putchar(43);
 		return (1);
-	}else if (n < 0)
-        {
-                _putchar(45);
-                return (-1);
-        }
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
 	else
 	{
 		_putchar(48);
